99028ef05fb9c604f08b4910c1f20a5efde01fc7
Andreas Wilcox <unknownbum@gmail.com>
2015-07-26 01:35:28 +0200

    Made basic dig effect

---
+++ Assets/MonoBehaviourExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public static class MonoBehaviourExtensions
+{
+    public static void Invoke(this MonoBehaviour monoBehaviour, Action action, float delayTime)
+    {
+        monoBehaviour.Invoke(action.Method.Name, delayTime);
+    }
+}
---
+++ Assets/MouseDigEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDigEffect : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+
+    void Start ()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _particleSystem.Play();
+        }
+    }
+}
