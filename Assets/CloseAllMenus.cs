eb2e2d713725deef8d45ce7fa217a63c39e4e9f6
Henrik Walldén <henrik@wallden.co>
2015-08-26 22:23:26 +0200

    andreas aer jetesnel

--- Assets/CloseAllMenus.cs
+++ Assets/CloseAllMenus.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class CloseAllMenus : MonoBehaviour {
+public class CloseAllMenus : MonoBehaviour
+{
 
-    public RectTransform InGameStoreRectTransform;
-    private RectTransform LocalRect { get; set; }
+	public RectTransform InGameStoreRectTransform;
+	private RectTransform LocalRect { get; set; }
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-        LocalRect = GetComponent<RectTransform>();
-        LocalRect.position = new Vector3(Screen.width/2f,Screen.height);
-    }
+		//LocalRect = GetComponent<RectTransform>();
+		//LocalRect.position = new Vector3(Screen.width/2f,Screen.height);
+	}
 
-   
 
-    // Update is called once per frame
-	void Update () {
-	   LocalRect.sizeDelta = new Vector3(Screen.width, (Screen.height/1.1f));
+
+	// Update is called once per frame
+	void Update()
+	{
+		//  LocalRect.sizeDelta = new Vector3(Screen.width, (Screen.height/1.1f));
 	}
 }
--- Assets/StorePurchase.cs
+++ Assets/StorePurchase.cs
@@ -1,83 +1,81 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using Soomla;
+﻿using System.Collections.Generic;
 using Soomla.Store;
-
+using UnityEngine;
 using UnityEngine.UI;
 
 public class StorePurchase : MonoBehaviour
 {
 
-    public Text StatusText;
-    public Text CurrentInGameCurrency;
+	//public Text StatusText;
+	public Text CurrentInGameCurrency;
 
-    void Start()
-    {
+	void Start()
+	{
 
-        SoomlaStore.Initialize(new Store());
+		SoomlaStore.Initialize(new Store());
 
-        StoreEvents.OnMarketPurchaseStarted += OnMarketPurchaseStarted;
-        StoreEvents.OnMarketPurchase += OnMarketPurchase;
-        StoreEvents.OnItemPurchaseStarted += OnItemPurchaseStarted;
-        StoreEvents.OnItemPurchased += OnItemPurchased;
-    }
+		StoreEvents.OnMarketPurchaseStarted += OnMarketPurchaseStarted;
+		StoreEvents.OnMarketPurchase += OnMarketPurchase;
+		StoreEvents.OnItemPurchaseStarted += OnItemPurchaseStarted;
+		StoreEvents.OnItemPurchased += OnItemPurchased;
+	}
 
-    string s = "<nothing>";
+	string s = "<nothing>";
 
-    public void OnMarketPurchaseStarted(PurchasableVirtualItem pvi)
-    {
-        StatusText.text = "Market Purchase started";
+	public void OnMarketPurchaseStarted(PurchasableVirtualItem pvi)
+	{
+		//StatusText.text = "Market Purchase started";
 
-        Debug.Log("OnMarketPurchaseStarted: " + pvi.ItemId);
-        s += "OnMarketPurchaseStarted: " + pvi.ItemId;
-    }
+		Debug.Log("OnMarketPurchaseStarted: " + pvi.ItemId);
+		s += "OnMarketPurchaseStarted: " + pvi.ItemId;
+	}
 
-    public void OnMarketPurchase(PurchasableVirtualItem pvi, string s1, Dictionary<string, string> arg3)
-    {
-        Debug.Log("OnMarketPurchase: " + pvi.ItemId);
-        s += "OnMarketPurchase: " + pvi.ItemId;
-        StatusText.text = s;
-    }
+	public void OnMarketPurchase(PurchasableVirtualItem pvi, string s1, Dictionary<string, string> arg3)
+	{
+		Debug.Log("OnMarketPurchase: " + pvi.ItemId);
+		s += "OnMarketPurchase: " + pvi.ItemId;
+		//StatusText.text = s;
+	}
 
-    public void OnItemPurchaseStarted(PurchasableVirtualItem pvi)
-    {
-        StatusText.text = "Item Purchase started";
+	public void OnItemPurchaseStarted(PurchasableVirtualItem pvi)
+	{
+		//StatusText.text = "Item Purchase started";
 
-        Debug.Log("OnItemPurchaseStarted: " + pvi.ItemId);
-        s += "OnItemPurchaseStarted: " + pvi.ItemId;
-        
-    }
+		Debug.Log("OnItemPurchaseStarted: " + pvi.ItemId);
+		s += "OnItemPurchaseStarted: " + pvi.ItemId;
 
-    public void OnItemPurchased(PurchasableVirtualItem pvi, string s1)
-    {
-        Debug.Log("OnItemPurchased: " + pvi.ItemId);
-        s += "OnItemPurchased: " + pvi.ItemId;
-        StatusText.text = s;
-        CurrentInGameCurrency.text = Store.DIAMOND_CURRENCY.GetBalance().ToString() + " Diamonds";
-    }
+	}
 
-    public void OnStoreControllerInitialized()
-    {
-        Debug.Log("OnStoreControllerInitialized");
-        s += "OnStoreControllerInitialized";
-        StatusText.text = s;
-    }
+	public void OnItemPurchased(PurchasableVirtualItem pvi, string s1)
+	{
+		Debug.Log("OnItemPurchased: " + pvi.ItemId);
+		s += "OnItemPurchased: " + pvi.ItemId;
+		//StatusText.text = s;
+		CurrentInGameCurrency.text = Store.DIAMOND_CURRENCY.GetBalance().ToString();
+	}
 
-    public void OnUnexpectedErrorInStore(string err)
-    {
-        Debug.Log("OnUnexpectedErrorInStore" + err);
-        s += "OnUnexpectedErrorInStore" + err;
-    }
+	public void OnStoreControllerInitialized()
+	{
+		Debug.Log("OnStoreControllerInitialized");
+		s += "OnStoreControllerInitialized";
+		//StatusText.text = s;
+	}
 
-    public void PurchaseCurrency(string amount)
-    {
-        //Store.HUND_DIAMONDS_PACK.Buy(amount);
+	public void OnUnexpectedErrorInStore(string err)
+	{
+		Debug.Log("OnUnexpectedErrorInStore" + err);
+		s += "OnUnexpectedErrorInStore" + err;
+	}
 
-        StoreInventory.BuyItem(Store.HUND_DIAMONDS_PACK.ItemId);
-    
-    }
-    public void PurchaseTimeMachine(string amount)
-    {
-        StoreInventory.BuyItem(Store.NO_ADS_LTVG.ItemId);
-    }
+	public void PurchaseCurrency(string amount)
+	{
+		//Store.HUND_DIAMONDS_PACK.Buy(amount);
+
+		StoreInventory.BuyItem(Store.HUND_DIAMONDS_PACK.ItemId);
+
+	}
+	public void PurchaseTimeMachine(string amount)
+	{
+		StoreInventory.BuyItem(Store.NO_ADS_LTVG.ItemId);
+	}
 }
